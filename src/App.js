72d4c41cee504b5a96a5b1bcf9a3d5a8d7bc34a0
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-28 17:53:04 +0100

    feat: race conditions with componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,42 +1,112 @@
-import React, { Suspense } from "react";
-import { fetchProfileData } from "./fakeApi";
+import React, { Component } from "react";
+import { fetchUser, fetchPosts, getNextId } from "./fakeApi";
 
-const resource = fetchProfileData();
+class ProfilePage extends Component {
+  state = {
+    user: null
+  };
 
-function ProfileDetails() {
-  const user = resource.user.read();
-  return <h1>{user.name}</h1>;
+  componentDidMount() {
+    this.fetchData(this.props.id);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.id !== this.props.id) {
+      this.fetchData(this.props.id);
+    }
+  }
+
+  async fetchData(id) {
+    const user = await fetchUser(id)
+      .then(res => res)
+      .catch(() => null);
+
+    return this.setState({ user });
+  }
+
+  render() {
+    const { id } = this.props;
+    const { user } = this.state;
+    if (user === null) {
+      return <h1 className="lead">Loading Profile...</h1>;
+    }
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <ProfileTimeLine id={id} />
+      </>
+    );
+  }
 }
 
-function ProfileTimeLine() {
-  const posts = resource.posts.read();
-  return (
-    <ul>
-      {posts.map(({ id, text }) => (
-        <li key={id}>{text}</li>
-      ))}
-    </ul>
-  );
+class ProfileTimeLine extends Component {
+  state = {
+    posts: null
+  };
+
+  componentDidMount() {
+    this.fetchData(this.props.id);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.id !== this.props.id) {
+      this.fetchData(this.props.id);
+    }
+  }
+
+  async fetchData(id) {
+    const posts = await fetchPosts(id)
+      .then(res => res)
+      .catch(() => null);
+
+    return this.setState({ posts });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    if (posts === null) {
+      return <h1 className="lead">Loading Posts...</h1>;
+    }
+    return (
+      <ul>
+        {posts.map(({ id, text }) => (
+          <li key={id}>{text}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
-function ProfilePage() {
-  return (
-    <Suspense fallback={<h1 className="lead">Loading Profile...</h1>}>
-      <ProfileDetails />
-      <Suspense fallback={<h1 className="lead">Loading Posts...</h1>}>
-        <ProfileTimeLine />
-      </Suspense>
-    </Suspense>
-  );
-}
+class App extends Component {
+  state = {
+    id: 0
+  };
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className="display-2">Experimental</h1>
-      <ProfilePage />
-    </div>
-  );
+  handleClick = () => {
+    const next = getNextId(this.state.id);
+    return this.setState({ id: next });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1 className="display-2">Experimental</h1>
+          <ProfilePage id={this.state.id} />
+        </div>
+        <div className="fab-bottom">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.handleClick}
+          >
+            Next
+          </button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
