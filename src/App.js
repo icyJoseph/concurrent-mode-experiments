5a21f83d7791b1374b0385e31dcb42a83b88da41
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-29 08:50:14 +0100

    feat: initiate concurrent mode, with createRoot

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Suspense } from "react";
-import { fetchProfileData } from "./fakeApi";
+import React, { Suspense, useState } from "react";
+import { fetchProfileData, getNextId } from "./fakeApi";
 
-const resource = fetchProfileData();
+const initialResource = fetchProfileData(0);
 
-function ProfileDetails() {
+function ProfileDetails({ resource }) {
+  // throws Promise, and lets its fiber know that it is pending data
   const user = resource.user.read();
   return <h1>{user.name}</h1>;
 }
 
-function ProfileTimeLine() {
+function ProfileTimeLine({ resource }) {
+  // throws Promise, and lets its fiber know that it is pending data
   const posts = resource.posts.read();
   return (
     <ul>
@@ -19,23 +21,39 @@ function ProfileTimeLine() {
   );
 }
 
-function ProfilePage() {
+function ProfilePage({ resource }) {
   return (
     <Suspense fallback={<h1 className="lead">Loading Profile...</h1>}>
-      <ProfileDetails />
+      <ProfileDetails resource={resource} />
       <Suspense fallback={<h1 className="lead">Loading Posts...</h1>}>
-        <ProfileTimeLine />
+        <ProfileTimeLine resource={resource} />
       </Suspense>
     </Suspense>
   );
 }
 
 function App() {
+  // Because, we now have the posts and user resources, inside resource
+  // move the resource to the top level
+  const [resource, setResource] = useState(initialResource);
+
+  const handleClick = () => {
+    const next = getNextId(resource.userId);
+    return setResource(fetchProfileData(next));
+  };
+
   return (
-    <div className="container">
-      <h1 className="display-2">Experimental</h1>
-      <ProfilePage />
-    </div>
+    <>
+      <div className="container">
+        <h1 className="display-2">Experimental</h1>
+        <ProfilePage resource={resource} />
+      </div>
+      <div className="fab-bottom">
+        <button type="button" className="btn btn-primary" onClick={handleClick}>
+          Next
+        </button>
+      </div>
+    </>
   );
 }
 
--- src/fakeApi.js
+++ src/fakeApi.js
@@ -1,7 +1,8 @@
-export function fetchProfileData() {
-  let userPromise = fetchUser();
-  let postsPromise = fetchPosts();
+export function fetchProfileData(userId) {
+  let userPromise = fetchUser(userId);
+  let postsPromise = fetchPosts(userId);
   return {
+    userId,
     user: wrapPromise(userPromise),
     posts: wrapPromise(postsPromise)
   };
@@ -37,37 +38,116 @@ function wrapPromise(promise) {
   };
 }
 
-function fetchUser() {
-  console.log("fetch user...");
+export function getNextId(id) {
+  return id === 3 ? 0 : id + 1;
+}
+
+export function fetchUser(userId) {
+  console.log("fetch user " + userId + "...");
   return new Promise(resolve => {
     setTimeout(() => {
-      console.log("fetched user");
-      resolve({
-        name: "Ringo Starr"
-      });
-    }, 1000);
+      console.log("fetched user " + userId);
+      switch (userId) {
+        case 0:
+          resolve({
+            name: "Ringo Starr"
+          });
+          break;
+        case 1:
+          resolve({
+            name: "George Harrison"
+          });
+          break;
+        case 2:
+          resolve({
+            name: "John Lennon"
+          });
+          break;
+        case 3:
+          resolve({
+            name: "Paul McCartney"
+          });
+          break;
+        default:
+          throw Error("Unknown user.");
+      }
+    }, 2000 * Math.random());
   });
 }
 
-function fetchPosts() {
-  console.log("fetch posts...");
+export function fetchPosts(userId) {
+  console.log("fetch posts for " + userId + "...");
   return new Promise(resolve => {
     setTimeout(() => {
-      console.log("fetched posts");
-      resolve([
-        {
-          id: 0,
-          text: "I get by with a little help from my friends"
-        },
-        {
-          id: 1,
-          text: "I'd like to be under the sea in an octupus's garden"
-        },
-        {
-          id: 2,
-          text: "You got that sand all over your feet"
-        }
-      ]);
-    }, 2000);
+      console.log("fetched posts for " + userId);
+      switch (userId) {
+        case 0:
+          resolve([
+            {
+              id: 0,
+              text: "I get by with a little help from my friends"
+            },
+            {
+              id: 1,
+              text: "I'd like to be under the sea in an octupus's garden"
+            },
+            {
+              id: 2,
+              text: "You got that sand all over your feet"
+            }
+          ]);
+          break;
+        case 1:
+          resolve([
+            {
+              id: 0,
+              text: "Turn off your mind, relax, and float downstream"
+            },
+            {
+              id: 1,
+              text: "All things must pass"
+            },
+            {
+              id: 2,
+              text: "I look at the world and I notice it's turning"
+            }
+          ]);
+          break;
+        case 2:
+          resolve([
+            {
+              id: 0,
+              text: "Living is easy with eyes closed"
+            },
+            {
+              id: 1,
+              text: "Nothing's gonna change my world"
+            },
+            {
+              id: 2,
+              text: "I am the walrus"
+            }
+          ]);
+          break;
+        case 3:
+          resolve([
+            {
+              id: 0,
+              text: "Woke up, fell out of bed"
+            },
+            {
+              id: 1,
+              text: "Here, there, and everywhere"
+            },
+            {
+              id: 2,
+              text: "Two of us sending postcards, writing letters"
+            }
+          ]);
+          break;
+        default:
+          throw Error("Unknown user.");
+      }
+    }, 2000 * Math.random());
   });
 }
