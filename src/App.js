da52e224ac67eb58d636bd5df4ccfe6000e8e354
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-29 07:14:06 +0100

    chore: add description and comments for render-as-you-fetch strategy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Suspense } from "react";
 import { fetchProfileData } from "./fakeApi";
 
+// This is not a Promise. It's a special object from our Suspense integration.
 const resource = fetchProfileData();
 
+// With Suspense, we don’t wait for the response to come back before we start rendering.
+// In fact, we start rendering pretty much immediately after kicking off the network request:
 function ProfileDetails() {
   const user = resource.user.read();
   return <h1>{user.name}</h1>;
 }
 
+/**
+ * @description calling on read, throws a Promise, which is caught by React's Suspense boundary
+ */
+
+// Same here!
 function ProfileTimeLine() {
   const posts = resource.posts.read();
   return (
