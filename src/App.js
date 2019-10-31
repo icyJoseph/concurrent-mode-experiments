e68ef5182a8abedba65bee5e3a41dfc9ddc64a9e
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-31 17:35:09 +0100

    feat: setup trivia example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Suspense, useState } from "react";
-import { fetchProfileData, getNextId } from "./fakeApi";
+import React, { Suspense } from "react";
+import { fetchProfileData } from "./fakeApi";
 
 const initialResource = fetchProfileData(0);
 
@@ -21,39 +21,42 @@ function ProfileTimeLine({ resource }) {
   );
 }
 
+function ProfileTrivia({ resource }) {
+  const trivia = resource.trivia.read();
+  return (
+    <>
+      <h2>Fun Facts</h2>
+      <ul>
+        {trivia.map(fact => (
+          <li key={fact.id}>{fact.text}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
 function ProfilePage({ resource }) {
   return (
-    <Suspense fallback={<h1 className="lead">Loading Profile...</h1>}>
+    <>
       <ProfileDetails resource={resource} />
       <Suspense fallback={<h1 className="lead">Loading Posts...</h1>}>
         <ProfileTimeLine resource={resource} />
       </Suspense>
-    </Suspense>
+      <Suspense fallback={<h1 className="lead"> Loading fun facts...</h1>}>
+        <ProfileTrivia resource={resource} />
+      </Suspense>
+    </>
   );
 }
 
 function App() {
-  // Because, we now have the posts and user resources, inside resource
-  // move the resource to the top level
-  const [resource, setResource] = useState(initialResource);
-
-  const handleClick = () => {
-    const next = getNextId(resource.userId);
-    return setResource(fetchProfileData(next));
-  };
-
   return (
-    <>
-      <div className="container">
-        <h1 className="display-2">Experimental</h1>
-        <ProfilePage resource={resource} />
-      </div>
-      <div className="fab-bottom">
-        <button type="button" className="btn btn-primary" onClick={handleClick}>
-          Next
-        </button>
-      </div>
-    </>
+    <div className="container">
+      <h1 className="display-2">Experimental</h1>
+      <Suspense fallback={<h1 className="lead">Loading Profile.</h1>}>
+        <ProfilePage resource={initialResource} />
+      </Suspense>
+    </div>
   );
 }
 
--- src/fakeApi.js
+++ src/fakeApi.js
@@ -1,10 +1,11 @@
-export function fetchProfileData(userId) {
-  let userPromise = fetchUser(userId);
-  let postsPromise = fetchPosts(userId);
+export function fetchProfileData() {
+  let userPromise = fetchUser();
+  let postsPromise = fetchPosts();
+  let triviaPromise = fetchTrivia();
   return {
-    userId,
     user: wrapPromise(userPromise),
-    posts: wrapPromise(postsPromise)
+    posts: wrapPromise(postsPromise),
+    trivia: wrapPromise(triviaPromise)
   };
 }
 
@@ -38,116 +39,62 @@ function wrapPromise(promise) {
   };
 }
 
-export function getNextId(id) {
-  return id === 3 ? 0 : id + 1;
-}
-
-export function fetchUser(userId) {
-  console.log("fetch user " + userId + "...");
+function fetchUser() {
+  console.log("fetch user...");
   return new Promise(resolve => {
     setTimeout(() => {
-      console.log("fetched user " + userId);
-      switch (userId) {
-        case 0:
-          resolve({
-            name: "Ringo Starr"
-          });
-          break;
-        case 1:
-          resolve({
-            name: "George Harrison"
-          });
-          break;
-        case 2:
-          resolve({
-            name: "John Lennon"
-          });
-          break;
-        case 3:
-          resolve({
-            name: "Paul McCartney"
-          });
-          break;
-        default:
-          throw Error("Unknown user.");
-      }
-    }, 2000 * Math.random());
+      console.log("fetched user");
+      resolve({
+        name: "Ringo Starr"
+      });
+    }, 500);
   });
 }
 
-export function fetchPosts(userId) {
-  console.log("fetch posts for " + userId + "...");
+let ringoPosts = [
+  {
+    id: 0,
+    text: "I get by with a little help from my friends"
+  },
+  {
+    id: 1,
+    text: "I'd like to be under the sea in an octupus's garden"
+  },
+  {
+    id: 2,
+    text: "You got that sand all over your feet"
+  }
+];
+
+function fetchPosts() {
+  let ringoPostsAtTheTime = ringoPosts;
+  console.log("fetch posts...");
   return new Promise(resolve => {
     setTimeout(() => {
-      console.log("fetched posts for " + userId);
-      switch (userId) {
-        case 0:
-          resolve([
-            {
-              id: 0,
-              text: "I get by with a little help from my friends"
-            },
-            {
-              id: 1,
-              text: "I'd like to be under the sea in an octupus's garden"
-            },
-            {
-              id: 2,
-              text: "You got that sand all over your feet"
-            }
-          ]);
-          break;
-        case 1:
-          resolve([
-            {
-              id: 0,
-              text: "Turn off your mind, relax, and float downstream"
-            },
-            {
-              id: 1,
-              text: "All things must pass"
-            },
-            {
-              id: 2,
-              text: "I look at the world and I notice it's turning"
-            }
-          ]);
-          break;
-        case 2:
-          resolve([
-            {
-              id: 0,
-              text: "Living is easy with eyes closed"
-            },
-            {
-              id: 1,
-              text: "Nothing's gonna change my world"
-            },
-            {
-              id: 2,
-              text: "I am the walrus"
-            }
-          ]);
-          break;
-        case 3:
-          resolve([
-            {
-              id: 0,
-              text: "Woke up, fell out of bed"
-            },
-            {
-              id: 1,
-              text: "Here, there, and everywhere"
-            },
-            {
-              id: 2,
-              text: "Two of us sending postcards, writing letters"
-            }
-          ]);
-          break;
-        default:
-          throw Error("Unknown user.");
-      }
-    }, 2000 * Math.random());
+      console.log("fetched posts");
+      resolve(ringoPostsAtTheTime);
+    }, 3000 * Math.random());
+  });
+}
+
+function fetchTrivia() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          text:
+            'The nickname "Ringo" came from his habit of wearing numerous rings.'
+        },
+        {
+          id: 2,
+          text: "Plays the drums left-handed with a right-handed drum set."
+        },
+        {
+          id: 3,
+          text: "Nominated for one Daytime Emmy Award, but did not win"
+        }
+      ]);
+    }, 3000 * Math.random());
   });
 }
