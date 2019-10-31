46b8054bf2a311ac06a884a8ae91c0a0b516da88
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-31 17:22:43 +0100

    feat: set up slow list without useDeferredValue

--- src/App.js
+++ src/App.js
@@ -1,76 +1,21 @@
-import React, {
-  Suspense,
-  useState,
-  useTransition,
-  useDeferredValue
-} from "react";
-import { fetchProfileData, getNextId } from "./fakeApi";
-
-const initialResource = fetchProfileData(0);
-
-function ProfileDetails({ resource }) {
-  // throws Promise, and lets its fiber know that it is pending data
-  const user = resource.user.read();
-  return <h1>{user.name}</h1>;
-}
-
-function ProfileTimeLine({ resource, isStale }) {
-  // throws Promise, and lets its fiber know that it is pending data
-  const posts = resource.posts.read();
-  return (
-    <ul style={{ opacity: isStale ? 0.3 : 1 }}>
-      {posts.map(({ id, text }) => (
-        <li key={id} className="lead">{text}</li>
-      ))}
-    </ul>
-  );
-}
-
-function ProfilePage({ resource }) {
-  const deferredResource = useDeferredValue(resource, { timeoutMs: 1000 });
-  return (
-    <Suspense fallback={<h1 className="lead">Loading Profile...</h1>}>
-      <ProfileDetails resource={resource} />
-      <Suspense fallback={<h1 className="lead">Loading Posts...</h1>}>
-        <ProfileTimeLine
-          resource={deferredResource}
-          isStale={deferredResource !== resource}
-        />
-      </Suspense>
-    </Suspense>
-  );
-}
+import React, { useState } from "react";
+import SlowList from "./SlowList";
 
 function App() {
-  // Because, we now have the posts and user resources, inside resource
-  // move the resource to the top level
-  const [resource, setResource] = useState(initialResource);
-  const [startTransition, isPending] = useTransition({ timeoutMs: 3000 });
+  const [text, setText] = useState("hello");
 
-  const handleClick = () => {
-    startTransition(() => {
-      const next = getNextId(resource.userId);
-      return setResource(fetchProfileData(next));
-    });
-  };
+  function handleChange(e) {
+    setText(e.target.value);
+  }
 
   return (
-    <>
-      <div className="container">
-        <h1 className="display-2">Experimental</h1>
-        <ProfilePage resource={resource} />
-      </div>
-      <div className="fab-bottom">
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleClick}
-          disabled={isPending}
-        >
-          Next
-        </button>
-      </div>
-    </>
+    <div className="container">
+      <h1 className="display-2">Experimental</h1>
+      <label>Type something: </label>
+      <input value={text} onChange={handleChange} />
+      <br />
+      <SlowList text={text} />
+    </div>
   );
 }
 
